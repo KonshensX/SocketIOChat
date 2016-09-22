57a20769869805b4b2fb77b147a6b452827e4c55
KonshensX <mohamedbaza16@gmail.com>
2016-09-22 13:43:01 +0100

    Initial Commit
    Making this stuff available

---
+++ server.js
@@ -0,0 +1,39 @@
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io').listen(server);
+users = [];
+connections = [];
+
+app.use('/assets', express.static(__dirname+'/public'));
+
+server.listen(process.env.PORT || 3000)
+console.log("Sever is up and running");
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+
+//Socket.IO stuff
+io.sockets.on('connection', function(socket){
+  connections.push(socket);
+  console.log("User joined the chat: %s sockets connected", connections.length);
+
+  //When disconnected
+  socket.on('disconnect', function(data){
+    connections.splice(connections.indexOf(socket), 1);
+    console.log("User disconnected: %s sockets connected", connections.length);
+  });
+
+  //send message
+  socket.on('send message', function(data){
+    io.sockets.emit('new message', {msg: data});
+  });
+
+  //new user
+  socket.on('user joined', function(data){
+    users.push(data);
+    io.socket.emit('new user', {users: users});
+  });
+});
